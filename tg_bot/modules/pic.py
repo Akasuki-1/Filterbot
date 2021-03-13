fd60bd9b1ae5981022beba2f4a9cb63bec37c140
Test account <79846938+Akasuki-1@users.noreply.github.com>
2021-03-13 13:34:13 +0530

    Update pic.py

--- tg_bot/modules/pic.py
+++ tg_bot/modules/pic.py
@@ -28,9 +28,8 @@ __mod_name__ = "PICTURE"
 
 
 __help__ = """
-   update.effective_message.reply_photo(
-            QWERTUR, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
-        )
+  
+reply_photo(QWERTUR, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
   - /afk <reason>: Mark yourself as AFK.
   - brb <reason>: Same as the afk command, but not a command.\n
   When marked as AFK, any mentions will be replied to with a message stating that you're not available!
