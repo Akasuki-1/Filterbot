500bb2dc7e969d16bcced9598c9992141abc61b9
Test account <79846938+Akasuki-1@users.noreply.github.com>
2021-03-13 13:47:30 +0530

    Update pic.py

--- tg_bot/modules/pic.py
+++ tg_bot/modules/pic.py
@@ -20,12 +20,61 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 QWERTUR = "https://telegra.ph/file/ff1e6ce577cbc289fc321.jpg"
 
+@run_async
+def jokerr(bot: Bot, update: Update):
+    user = update.effective_message.from_user
+    chat = update.effective_chat  # type: Optional[Chat]
+
+    if chat.type == "private":
+        update.effective_message.reply_text(QWERTUR, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+
+        if OWNER_ID != 254318997 and DONATION_LINK:
+            update.effective_message.reply_photo(QWERTUR, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+
+                                            
+    else:
+        try:
+            bot.send_message(user.id, DONATE_STRING, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+
+            update.effective_message.reply_text("I've PM'ed you about donating to my creator!")
+        except Unauthorized:
+            update.effective_message.reply_text("Contact me in PM first to get donation information.")
+
+
+def migrate_chats(bot: Bot, update: Update):
+    msg = update.effective_message  # type: Optional[Message]
+    if msg.migrate_to_chat_id:
+        old_chat = update.effective_chat.id
+        new_chat = msg.migrate_to_chat_id
+    elif msg.migrate_from_chat_id:
+        old_chat = msg.migrate_from_chat_id
+        new_chat = update.effective_chat.id
+    else:
+        return
+
+    LOGGER.info("Migrating from %s, to %s", str(old_chat), str(new_chat))
+    for mod in MIGRATEABLE:
+        mod.__migrate__(old_chat, new_chat)
+
+    LOGGER.info("Successfully migrated!")
+    raise DispatcherHandlerStop
+
+
+def main():
+    
+    appchi_handler = CommandHandler("jokerr", jokerr)
+    
+    # dispatcher.add_handler(test_handler)
+    dispatcher.add_handler(jokerr_handler)
+
+
+
 __mod_name__ = "PICTURE"
 
 
 __help__ = """
   
-  - /afk <reason>: Mark yourself as AFk
+  - /jokerr <reason>: Mark yourself as AFk
   - brb <reason>: Same as the afk command, but not a command.\n
   When marked as AFK, any mentions will be replied to with a message stating that you're not available!
 """
