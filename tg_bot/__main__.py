5dd05572385c39360600b71a20f07aa3e18957f6
Test account <79846938+Akasuki-1@users.noreply.github.com>
2021-03-13 17:08:09 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,14 +18,11 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-
 Hello {},My Name is {} !. 
-
-```picture``` - /a /b /c /d /e /f .
-
-I'm Filter Manager Bot Maintained By {} {}. 
-
+Picturez - /a /b /c /d /e /f
+I'm Filter Manager Bot Maintained By [{}](https://t.me/{}). 
 """
+
 
 HELP_STRINGS = """
 Hello! my name is *{}*.
