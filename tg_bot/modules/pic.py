781ab2bb026c2f95e6c16041c41b1611f0a268b5
Test account <79846938+Akasuki-1@users.noreply.github.com>
2021-03-13 13:32:15 +0530

    Update pic.py

--- tg_bot/modules/pic.py
+++ tg_bot/modules/pic.py
@@ -3,6 +3,22 @@ import re
 from typing import Optional, List
 from sys import argv
 
+from telegram import Message, Chat, Update, Bot, User
+from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.error import Unauthorized, BadRequest, TimedOut, NetworkError, ChatMigrated, TelegramError
+from telegram.ext import CommandHandler, Filters, MessageHandler, CallbackQueryHandler
+from telegram.ext.dispatcher import run_async, DispatcherHandlerStop
+from telegram.utils.helpers import escape_markdown
+
+from tg_bot import dispatcher, updater, TOKEN, WEBHOOK, OWNER_ID, DONATION_LINK, CERT_PATH, PORT, URL, LOGGER, \
+    ALLOW_EXCL, START_PHOTTO, OWNER_NAME, OWNER_USERNAME 
+# needed to dynamically load modules
+# NOTE: Module order is not guaranteed, specify that in the config file!
+from tg_bot.modules import ALL_MODULES
+from tg_bot.modules.helper_funcs.chat_status import is_user_admin
+from tg_bot.modules.helper_funcs.misc import paginate_modules
+
+
 from telegram import Bot, Update, ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
 
 
