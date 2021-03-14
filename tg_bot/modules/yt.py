b8de220c9695d388d4804b7717ea3befb62795f7
Test account <79846938+Akasuki-1@users.noreply.github.com>
2021-03-14 17:34:42 +0530

    Create yt.py

---
+++ tg_bot/modules/yt.py
@@ -0,0 +1,6 @@
+__help__ = """
+ - /yt <text>: perform a youtube search
+ - /ytaudio <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local server
+ -             ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ  ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤmade byㅤㅤㅤㅤㅤ@the_noobhacker
+"""
+__mod_name__ = "Youtube 🎞"
