d8327234a5aff54ee56a8f27ec2a353b49c8c421
Test account <79846938+Akasuki-1@users.noreply.github.com>
2021-03-14 09:39:42 +0530

    Update __aifunction.py

--- tg_bot/modules/__aifunction.py
+++ tg_bot/modules/__aifunction.py
@@ -1,7 +1,7 @@
 import requests
 url = "https://iamai.p.rapidapi.com/ask"
-from MashaRoBot import telethn, OWNER_ID
-from MashaRoBot.events import register
+from tg_bot import telethn, OWNER_ID
+from tg_bot.events import register
 from telethon import events
 from telethon import types
 from telethon.tl import functions
